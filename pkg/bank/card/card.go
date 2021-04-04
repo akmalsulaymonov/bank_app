a24950ed0ec137f0adfac343414b9adfe648ba08
Akmal Sulaymonov <akmalsulaymonov@gmail.com>
2021-04-04 11:58:12 +0500

    Added Withdraw function

---
+++ pkg/bank/card/card.go
@@ -0,0 +1,24 @@
+package card
+
+import "github.com/akmalsulaymonov87/bank_app/pkg/bank/types"
+
+func Withdraw(card *types.Card, amount types.Money) {
+	const withdrawLimit = 20_000_00
+	if amount < 0 {
+		return
+	}
+
+	if amount > withdrawLimit {
+		return
+	}
+
+	if !card.Active {
+		return
+	}
+
+	if card.Balance < amount {
+		return
+	}
+
+	card.Balance -= amount
+}
